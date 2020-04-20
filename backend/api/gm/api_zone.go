f8bd4f976f5587f1750fa1956c3ef400185b3b50
Yellia <865756738@qq.com>
2020-04-20 19:36:47 +0800

    1

--- backend/api/gm/api_zone.go
+++ backend/api/gm/api_zone.go
@@ -7,18 +7,22 @@ import (
     "github.com/yellia1989/tex-web/backend/api/gm/rpc"
 )
 
-func ZoneSimpleList(c echo.Context) error {
-    ctx := c.(*mid.Context)
-
+func zoneList(c echo.Context) ([]rpc.ZoneInfo) {
     dirPrx := new(rpc.DirService)
     comm.StringToProxy("aqua.DirServer.DirServiceObj", dirPrx)
 
     var zones []rpc.ZoneInfo
     ret, err := dirPrx.GetAllZone(&zones)
     if ret != 0 || err != nil {
-        ctx.SendError(-1, fmt.Sprintf("ret:%d, err:%s", ret, err.Error()))
+        c.Error(fmt.Errorf("get zone list failed, ret:%d, err:%s", ret, err.Error()))
     }
+    return zones
+}
 
+func ZoneSimpleList(c echo.Context) error {
+    ctx := c.(*mid.Context)
+
+    zones := zoneList(c)
     zones = append(zones, rpc.ZoneInfo{IZoneId: 0, SZoneName: "GFServer"})
 
     return ctx.SendResponse(zones)
@@ -27,14 +31,7 @@ func ZoneSimpleList(c echo.Context) error {
 func ZoneList(c echo.Context) error {
     ctx := c.(*mid.Context)
 
-    dirPrx := new(rpc.DirService)
-    comm.StringToProxy("aqua.DirServer.DirServiceObj", dirPrx)
-
-    var zones []rpc.ZoneInfo
-    ret, err := dirPrx.GetAllZone(&zones)
-    if ret != 0 || err != nil {
-        ctx.SendError(-1, fmt.Sprintf("ret:%d, err:%s", ret, err.Error()))
-    }
+    zones := zoneList(c)
 
     return ctx.SendResponse(zones)
 }
