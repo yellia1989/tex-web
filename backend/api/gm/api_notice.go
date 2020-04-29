672e7392e6151251ce5b2396d912a896774bcd14
LeiZhao123 <1719472077@qq.com>
2020-04-29 11:54:09 +0800

    1

---
+++ backend/api/gm/api_notice.go
@@ -0,0 +1,105 @@
+package gm
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/labstack/echo"
+	"github.com/yellia1989/tex-web/backend/api/gm/rpc"
+	"github.com/yellia1989/tex-web/backend/common"
+	mid "github.com/yellia1989/tex-web/backend/middleware"
+)
+
+func NoticeList(c echo.Context) error {
+	ctx := c.(*mid.Context)
+	page, _ := strconv.Atoi(ctx.QueryParam("page"))
+	limit, _ := strconv.Atoi(ctx.QueryParam("limit"))
+
+	bulletinPrx := new(rpc.BulletinService)
+	comm.StringToProxy("aqua.BulletinServer.BulletinServiceObj", bulletinPrx)
+
+	var vNotice []rpc.NoticeDataInfo
+	ret, err := bulletinPrx.GetAllNotice(&vNotice)
+	if err := checkRet(ret, err); err != nil {
+		return err
+	}
+
+	vPage := common.GetPage(vNotice, page, limit)
+	return ctx.SendArray(vPage, len(vNotice))
+}
+
+func NoticeAdd(c echo.Context) error {
+	ctx := c.(*mid.Context)
+
+	notice := rpc.NoticeDataInfo{}
+	if err := ctx.Bind(&notice); err != nil {
+		return err
+	}
+
+	/*
+			bulletinPrx := new(rpc.BulletinService)
+			comm.StringToProxy("aqua.BulletinServer.BulletinServiceObj", bulletinPrx)
+
+			ret, err := bulletinPrx.AddNotice()
+			if err := checkRet(ret, err); err != nil {
+				return err
+		    }
+	*/
+
+	return ctx.SendResponse("添加跑马灯成功")
+}
+
+func NoticeDel(c echo.Context) error {
+	ctx := c.(*mid.Context)
+
+	ids := strings.Split(ctx.FormValue("idsStr"), ",")
+
+	if len(ids) == 0 {
+		return ctx.SendError(-1, "跑马灯不存在")
+	}
+
+	/*
+			bulletinPrx := new(rpc.BulletinService)
+			comm.StringToProxy("aqua.BulletinServer.BulletinServiceObj", bulletinPrx)
+
+			for _, id := range ids {
+				id, _ := strconv.ParseUint(id, 10, 32)
+				ret, err := bulletinPrx.DelNotice(uint32(id))
+				if err := checkRet(ret, err); err != nil {
+					return err
+				}
+		    }
+	*/
+
+	return ctx.SendResponse("删除跑马灯成功")
+}
+
+func NoticeUpdate(c echo.Context) error {
+	ctx := c.(*mid.Context)
+
+	notice := rpc.NoticeDataInfo{}
+	if err := ctx.Bind(&notice); err != nil {
+		return err
+	}
+
+	/*
+			sTitle := ctx.FormValue("sTitle")
+			sContent := ctx.FormValue("sContent")
+			sBeginTime := ctx.FormValue("sBeginTime")
+			sEndTime := ctx.FormValue("sEndTime")
+
+			if sTitle == "" || sContent == "" || sBeginTime == "" || sEndTime == "" {
+				return ctx.SendError(-1, "参数非法")
+			}
+
+			bulletinPrx := new(rpc.BulletinService)
+			comm.StringToProxy("aqua.BulletinServer.BulletinServiceObj", bulletinPrx)
+
+			ret, err := bulletinPrx.ModifyNotice(notice)
+			if err := checkRet(ret, err); err != nil {
+				return err
+		    }
+	*/
+
+	return ctx.SendResponse("修改跑马灯成功")
+}
