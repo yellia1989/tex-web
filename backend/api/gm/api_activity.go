f360dd3ef40dd7b2008563c46c1fcdadd560acfb
LeiZhao123 <1719472077@qq.com>
2020-05-08 17:50:14 +0800

    2

--- backend/api/gm/api_activity.go
+++ backend/api/gm/api_activity.go
@@ -1,11 +1,37 @@
 package gm
 
 import (
+	"database/sql"
+	"strconv"
+
+	_ "github.com/go-sql-driver/mysql"
+
 	"github.com/labstack/echo"
 	mid "github.com/yellia1989/tex-web/backend/middleware"
 )
 
 func ActivityList(c echo.Context) error {
 	ctx := c.(*mid.Context)
-	return ctx.sendResponse("list")
+	db, err := sql.Open("mysql", "dev:777777@tcp(192.168.0.16)/db_loginserver")
+	defer db.Close()
+	if err != nil {
+		return err
+	}
+	rows, err := db.Query("SELECT * FROM t_whitelist;")
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	str := ""
+	for rows.Next() {
+		var id int
+		err = rows.Scan(&id)
+		if err != nil {
+			return err
+		}
+		str += strconv.Itoa(id)
+	}
+
+	return ctx.SendResponse(str)
 }
