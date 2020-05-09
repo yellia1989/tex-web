b44378754b91c3cec9a5321abfb40f509cd79218
leizhao <zhao.lei@vaneson.com>
2020-05-09 10:07:19 +0800

    2

--- backend/api/gm/api_whitelist.go
+++ backend/api/gm/api_whitelist.go
@@ -11,11 +11,13 @@ import (
 	mid "github.com/yellia1989/tex-web/backend/middleware"
 )
 
-func parseIDStr(src string, out *string) {
-	reg, _ := regexp.Compile("/[,;\r\n\t ]/")
+func parseIDStr(src, sep string, out *string) {
+	reg, _ := regexp.Compile("\\d{5}")
 	vStr := reg.FindAllString(src, -1)
 
-	*out = strings.Join(vStr, "),(")
+    if len(vStr) != 0 {
+	    *out = strings.Join(vStr, sep)
+    }
 }
 
 func WhiteList(c echo.Context) error {
@@ -54,19 +56,15 @@ func WhiteAdd(c echo.Context) error {
 		return ctx.SendError(-1, "参数非法")
 	}
 
-	db, err := sql.Open("mysql", "dev:777777@tcp(192.168.0.16)/db_loginserve?charset=utf8")
+	db, err := sql.Open("mysql", "dev:777777@tcp(192.168.0.16)/db_loginserver?charset=utf8")
 	defer db.Close()
 	if err != nil {
 		return err
 	}
 
-	stmt, err := db.Prepare("INSERT IGNORE INTO t_whitelist VALUES(?)")
-	if err != nil {
-		return err
-	}
-
-	parseIDStr(input, &input)
-	_, err = stmt.Exec(input)
+	parseIDStr(input, "),(", &input)
+    sql := "INSERT IGNORE INTO t_whitelist VALUES(" + input + ");"
+	_, err = db.Exec(sql)
 	if err != nil {
 		return err
 	}
@@ -83,19 +81,15 @@ func WhiteDel(c echo.Context) error {
 		return ctx.SendError(-1, "参数非法")
 	}
 
-	db, err := sql.Open("mysql", "dev:777777@tcp(192.168.0.16)/db_loginserve?charset=utf8")
+	db, err := sql.Open("mysql", "dev:777777@tcp(192.168.0.16)/db_loginserver?charset=utf8")
 	defer db.Close()
 	if err != nil {
 		return err
 	}
 
-	stmt, err := db.Prepare("DELETE FROM t_whitelist WHERE account_id IN(?)")
-	if err != nil {
-		return err
-	}
-
-	parseIDStr(input, &input)
-	_, err = stmt.Exec(input)
+	parseIDStr(input, ",", &input)
+    sql := "DELETE FROM t_whitelist WHERE account_id IN(" + input + ");"
+	_, err = db.Exec(sql)
 	if err != nil {
 		return err
 	}
@@ -108,20 +102,17 @@ func WhiteReplace(c echo.Context) error {
 
 	input := ctx.FormValue("input")
 
-	db, err := sql.Open("mysql", "dev:777777@tcp(192.168.0.16)/db_loginserve?charset=utf8")
+	db, err := sql.Open("mysql", "dev:777777@tcp(192.168.0.16)/db_loginserver?charset=utf8")
 	defer db.Close()
 	if err != nil {
 		return err
 	}
 
 	db.Exec("DELETE FROM t_whitelist;")
-	stmt, err := db.Prepare("INSERT IGNORE INTO t_whitelist VALUES(?)")
-	if err != nil {
-		return err
-	}
 
-	parseIDStr(input, &input)
-	_, err = stmt.Exec(input)
+	parseIDStr(input, "),(", &input)
+	sql := "INSERT IGNORE INTO t_whitelist VALUES(" + input + ");"
+	_, err = db.Exec(sql)
 	if err != nil {
 		return err
 	}
