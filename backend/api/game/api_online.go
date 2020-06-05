38240ff85b165e1e0353c84bc0efb71477e6c9d6
LeiZhao123 <1719472077@qq.com>
2020-06-05 16:44:19 +0800

    1

--- backend/api/game/api_online.go
+++ backend/api/game/api_online.go
@@ -20,8 +20,10 @@ func OnlineTime(c echo.Context) error {
 	zoneid := ctx.QueryParam("zoneid")
 	page, _ := strconv.Atoi(ctx.QueryParam("page"))
 	limit, _ := strconv.Atoi(ctx.QueryParam("limit"))
+	startTime := ctx.QueryParam("startTime")
+	endTime := ctx.QueryParam("endTime")
 
-	if zoneid == "" {
+	if zoneid == "" || startTime == "" || endTime == "" {
 		return ctx.SendError(-1, "参数非法")
 	}
 
@@ -44,7 +46,8 @@ func OnlineTime(c echo.Context) error {
 	limitstart := strconv.Itoa((page - 1) * limit)
 	limitrow := strconv.Itoa(limit)
 
-	sql := "SELECT roleid,sum(online_time) as online_time FROM logout GROUP BY roleid ORDER BY sum(online_time)"
+	sql := "SELECT roleid,sum(online_time) as online_time FROM logout "
+	sql += "WHERE time between '" + startTime + "' AND '" + endTime + "' GROUP BY roleid ORDER BY sum(online_time)"
 	sql += " LIMIT " + limitstart + "," + limitrow
 
 	rows, err := tx.Query(sql)
@@ -53,32 +56,81 @@ func OnlineTime(c echo.Context) error {
 	}
 	defer rows.Close()
 
-	logs := make([]_onlineTime, 0)
+	mRole := make(map[uint64]uint32)
 	for rows.Next() {
-		var r _onlineTime
-		/*
-			if err := rows.Scan(&r.StageID, &r.StageFirstStartNum, &firstPassNum, &totalPassNum); err != nil {
-				return err
-			}
-			r.StageFirstPassNum = uint32(firstPassNum.Int32)
-			r.StageTotalPassNum = uint32(totalPassNum.Int32)
+		var roleID uint64
+		var totalTime uint32
+		if err := rows.Scan(&roleID, &totalTime); err != nil {
+			return err
+		}
 
-			r.RoleNum = roleNum
-			r.StageFirstPassStar1Num = stage2StarNum[r.StageID].star1
-			r.StageFirstPassStar2Num = stage2StarNum[r.StageID].star2
-			r.StageFirstPassStar3Num = stage2StarNum[r.StageID].star3
-
-			r.StageLossRate = float32(r.StageFirstStartNum-r.StageTotalPassNum) / float32(r.StageFirstStartNum)
-			r.StageTotalLossRate = float32(roleNum-r.StageTotalPassNum) / float32(roleNum)
-		*/
-		logs = append(logs, r)
+		mRole[roleID] = totalTime
 	}
+
 	if err := rows.Err(); err != nil {
 		return err
 	}
 
 	if err := tx.Commit(); err != nil {
 		return err
+	}
+	roleNum := uint32(len(mRole))
+
+	// 初始化一套时间轴
+	times := map[uint32]uint32{300: 0, 600: 0, 1800: 0, 3600: 0, 7200: 0, 3600 * 4: 0, 3600 * 8: 0, 3600 * 12: 0, 3600 * 24: 0, 3600*24 + 1: 0}
+	// 统计数据
+	for _, t := range mRole {
+		switch {
+		case t <= 300:
+			times[300]++
+		case t <= 600:
+			times[600]++
+		case t <= 1800:
+			times[1800]++
+		case t <= 3600:
+			times[3600]++
+		case t <= 7200:
+			times[7200]++
+		case t <= 3600*4:
+			times[3600*4]++
+		case t <= 3600*8:
+			times[3600*8]++
+		case t <= 3600*12:
+			times[3600*12]++
+		case t <= 3600*24:
+			times[3600*24]++
+		default:
+			times[3600*24+1]++
+		}
+	}
+
+	logs := make([]_onlineTime, 0)
+	for t, num := range times {
+		r := _onlineTime{RoleNum: num, RoleNumTotal: roleNum}
+		switch {
+		case t == 300:
+			r.TotalTime = "0-5分钟"
+		case t == 600:
+			r.TotalTime = "5-10分钟"
+		case t == 1800:
+			r.TotalTime = "10-30分钟"
+		case t == 3600:
+			r.TotalTime = "30-60分钟"
+		case t == 7200:
+			r.TotalTime = "60-120分钟"
+		case t == 3600*4:
+			r.TotalTime = "2-4小时"
+		case t == 3600*8:
+			r.TotalTime = "4-8小时"
+		case t == 3600*12:
+			r.TotalTime = "8-12小时"
+		case t == 3600*24:
+			r.TotalTime = "12-24小时"
+		case t == 3600*24+1:
+			r.TotalTime = "24小时以上"
+		}
+		r.RoleNumRate = float32(roleNum / r.RoleNumTotal)
+		logs = append(logs, r)
 	}
 
 	return ctx.SendArray(logs, len(logs))
