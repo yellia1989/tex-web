410c9a53a41c42926592e2c236c22143502261e5
LeiZhao123 <1719472077@qq.com>
2020-06-10 16:38:45 +0800

    2

--- backend/api/gm/api_activity.go
+++ backend/api/gm/api_activity.go
@@ -2,18 +2,47 @@ package gm
 
 import (
 	"github.com/labstack/echo"
-	mid "github.com/yellia1989/tex-web/backend/middleware"
 	"github.com/yellia1989/tex-web/backend/common"
+	mid "github.com/yellia1989/tex-web/backend/middleware"
 )
 
 type _activityData struct {
 	ActivityID    uint32 `json:"activity_id"`
-	ActivityType    uint32 `json:"activity_type"`
+	ActivityType  uint32 `json:"activity_type"`
 	ApplyZone     string `json:"apply_zone"`
 	ApplyUser     string `json:"apply_user"`
 	ConfigureData string `json:"configure_data"`
 	ConfigureDesc string `json:"configure_desc"`
-    Ts string `json:"ts"`
+	Ts            string `json:"ts"`
+}
+
+type _activityType struct {
+	Type uint32 `json:"activity_type"`
+	Name string `json:"type_name"`
+}
+
+func ActivityTypeList(c echo.Context) []_activityData {
+	types := []_activityData{
+		{1, "模块控制"},
+		{2, "个人消耗"},
+		{3, "冲榜"},
+		{4, "累计充值"},
+		{5, "首冲送英雄"},
+		{6, "一元购"},
+		{7, "七日登陆"},
+		{8, "免费福利"},
+		{9, "成长基金"},
+		{10, "个性化活动"},
+		{11, "在线时长奖励"},
+		{12, "英雄众筹"},
+		{13, "许愿"},
+		{14, "英雄兑换"},
+		{15, "幸运转盘"},
+		{16, "邀请有礼"},
+		{17, "vip福利"},
+	}
+
+	return types
 }
 
 func ActivityList(c echo.Context) error {
