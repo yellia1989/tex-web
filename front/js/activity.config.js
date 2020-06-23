ba0c279d30236d6817e2b4e7b2cf3e125566f564
Yellia <865756738@qq.com>
2020-06-23 15:21:20 +0800

    1

--- front/js/activity.config.js
+++ front/js/activity.config.js
@@ -204,36 +204,6 @@
                 return activityType.fieldOption[fieldKey];
             }
             return undefined;
-        },
-        stringToDate: function(dateStr) {
-            var converted = Date.parse(dateStr);
-            var myDate = new Date(converted);
-            if (isNaN(myDate))
-            {
-                var arys= dateStr.split('-');
-                myDate = new Date(arys[0],--arys[1],arys[2]);
-            }
-            return myDate;
-        },
-        formatDate: function(fmt,d) {
-            var o = {
-                "M+": d.getMonth() + 1, //月份 
-                "d+": d.getDate(), //日 
-                "h+": d.getHours(), //小时 
-                "m+": d.getMinutes(), //分 
-                "s+": d.getSeconds(), //秒 
-                "q+": Math.floor((d.getMonth() + 3) / 3), //季度 
-                "S": d.getMilliseconds() //毫秒 
-            };
-            if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (d.getFullYear() + "").substr(4 - RegExp.$1.length));
-            for (var k in o)
-                if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-            return fmt;
-        },
-        addHour: function(dateStr, hour) {
-            var dt = this.stringToDate(dateStr);
-            var ndt = new Date(dt.getTime() + hour*3600*1000);
-            return this.formatDate('yyyy-MM-dd hh:mm:ss', ndt);
         }
     };
 
