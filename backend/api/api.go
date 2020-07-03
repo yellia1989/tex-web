0f5b0a883d64743c83d4b855c35d3134683aabbc
Yellia <865756738@qq.com>
2020-07-03 15:43:35 +0800

    1

--- backend/api/api.go
+++ backend/api/api.go
@@ -5,27 +5,28 @@ import (
 	"github.com/yellia1989/tex-web/backend/api/game"
 	"github.com/yellia1989/tex-web/backend/api/gm"
 	"github.com/yellia1989/tex-web/backend/api/stat"
+	"github.com/yellia1989/tex-web/backend/api/sys"
 )
 
 func RegisterHandler(group *echo.Group) {
 	group.POST("/login", UserLogin) // 登陆
 
-	group.GET("/menu/list", MenuList)               // 菜单列表
-	group.POST("/menu/update", MenuUpdate)          // 更新菜单
-	group.GET("/user/list", UserList)               // 用户列表
-	group.POST("/user/add", UserAdd)                // 用户增加
-	group.POST("/user/del", UserDel)                // 用户删除
-	group.POST("/user/update/role", UserUpdateRole) // 用户角色变更
-	group.POST("/user/update", UserUpdate)          // 用户更新
-	group.POST("/user/pwd", UserPwd)                // 用户改密码
-	group.GET("/perm/list", PermList)               // 权限列表
-	group.POST("/perm/add", PermAdd)                // 权限增加
-	group.POST("/perm/del", PermDel)                // 权限删除
-	group.POST("/perm/update", PermUpdate)          // 权限编辑
-	group.GET("/role/list", RoleList)               // 角色列表
-	group.POST("/role/add", RoleAdd)                // 角色增加
-	group.POST("/role/del", RoleDel)                // 角色删除
-	group.POST("/role/update", RoleUpdate)          // 角色更新
+	group.GET("/menu/list", sys.MenuList)               // 菜单列表
+	group.POST("/menu/update", sys.MenuUpdate)          // 更新菜单
+	group.GET("/user/list", sys.UserList)               // 用户列表
+	group.POST("/user/add", sys.UserAdd)                // 用户增加
+	group.POST("/user/del", sys.UserDel)                // 用户删除
+	group.POST("/user/update/role", sys.UserUpdateRole) // 用户角色变更
+	group.POST("/user/update", sys.UserUpdate)          // 用户更新
+	group.POST("/user/pwd", sys.UserPwd)                // 用户改密码
+	group.GET("/perm/list", sys.PermList)               // 权限列表
+	group.POST("/perm/add", sys.PermAdd)                // 权限增加
+	group.POST("/perm/del", sys.PermDel)                // 权限删除
+	group.POST("/perm/update", sys.PermUpdate)          // 权限编辑
+	group.GET("/role/list", sys.RoleList)               // 角色列表
+	group.POST("/role/add", sys.RoleAdd)                // 角色增加
+	group.POST("/role/del", sys.RoleDel)                // 角色删除
+	group.POST("/role/update", sys.RoleUpdate)          // 角色更新
 
 	group.POST("/gm/game/cmd", gm.GameCmd)               // 执行gm命令
 	group.GET("/gm/zone/simplelist", gm.ZoneSimpleList)  // 获取分区列表
