06ae72c25671a48781801b951ecb1675d101fe40
Yellia <865756738@qq.com>
2020-07-27 20:20:13 +0800

    增加ErrorCode

--- backend/api/gm/api_gm.go
+++ backend/api/gm/api_gm.go
@@ -80,7 +80,7 @@ func GameCmd(c echo.Context) error {
                 if err != nil {
                     serr = err.Error()
                 }
-                result = fmt.Sprintf("ret:%d, err:%s\n", ret, serr)
+                result = fmt.Sprintf("ret:%s, err:%s\n", rpc.ErrorCode(ret), serr)
             }
             buff.WriteString(result+"\n")
         }
