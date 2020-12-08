20ff96d712ce92b2ee59ef2de220eb4c5ab55840
Yellia <865756738@qq.com>
2020-12-08 10:31:21 +0800

    1

--- script/update_web.sh
+++ script/update_web.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -ne 1 ] ;then
-	echo "Usage: $0 env (d/47.103.96.228 u/139.224.58.141 t/101.132.43.124)"
+	echo "Usage: $0 env (d/47.103.96.228 u/139.224.58.141 r/47.91.45.105)"
 	exit 100
 fi
 
@@ -17,9 +17,9 @@ case "$env" in
     ip=139.224.58.141
     cp ../conf_u.cfg conf.cfg
     ;;
-    t)
-    ip=101.132.43.124
-    cp ../conf_t.cfg conf.cfg
+    r)
+    ip=47.91.45.105
+    cp ../conf_r.cfg conf.cfg
     ;;
     *)
     echo "invalid env"
