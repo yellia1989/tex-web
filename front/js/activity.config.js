b6a192016c878a1c9176e77a91c7dec2d4649cfc
LazyFishDream <1719472077@qq.com>
2021-02-25 10:59:09 +0800

    #2780

--- front/js/activity.config.js
+++ front/js/activity.config.js
@@ -326,6 +326,10 @@
                 recommond: {
                     name: '焦点参数(秒)',
                     type: 'text',
+                },
+                show_step: {
+                    name: '可显示档位',
+                    type: 'text',
                 }
             }
         }
