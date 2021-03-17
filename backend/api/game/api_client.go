b2d7b16fd48d959cd8403098637e5353db24cba4
lzhang <891056049@qq.com>
2021-03-17 11:38:07 +0800

    1.详细界面更新

--- backend/api/game/api_client.go
+++ backend/api/game/api_client.go
@@ -1,12 +1,13 @@
 package game
 
 import (
+	"sort"
+
 	"github.com/labstack/echo"
+	"github.com/yellia1989/tex-go/tools/log"
+	"github.com/yellia1989/tex-web/backend/cfg"
+	common "github.com/yellia1989/tex-web/backend/common"
 	mid "github.com/yellia1989/tex-web/backend/middleware"
-    common "github.com/yellia1989/tex-web/backend/common"
-    "github.com/yellia1989/tex-go/tools/log"
-    "github.com/yellia1989/tex-web/backend/cfg"
-    "sort"
 )
 
 type errSimpleInfo struct {
@@ -29,10 +30,10 @@ func (a errSimpleInfoBy) Less(i, j int) bool {
 }
 
 type errInfo struct {
-    ErrTime    string `json:"err_time"`
+	ErrTime    string `json:"err_time"`
 	ErrMessage string `json:"err_info"`
-    ZoneId     uint32 `json:"zone_id"`
-    RoleId     uint32 `json:"role_id"`
+	ZoneId     uint32 `json:"zone_id"`
+	RoleId     uint32 `json:"role_id"`
 }
 
 type errInfoBy []errInfo
@@ -45,70 +46,111 @@ func (a errInfoBy) Less(i, j int) bool {
 
 	return a[i].ErrMessage < a[j].ErrMessage
 
-    return a[i].ZoneId < a[j].ZoneId
+	return a[i].ZoneId < a[j].ZoneId
 }
 
 func ErrInfo(c echo.Context) error {
 	ctx := c.(*mid.Context)
-    startTime := ctx.QueryParam("startTime")
+	startTime := ctx.QueryParam("startTime")
 	endTime := ctx.QueryParam("endTime")
 
-    if startTime == "" || endTime == "" {
+	if startTime == "" || endTime == "" {
 		return ctx.SendError(-1, "参数非法")
 	}
 
-    db := cfg.LogDb
+	db := cfg.LogDb
 
-    sql := "SELECT time, message FROM client_err "
-    sql += "WHERE time BETWEEN '"+startTime+"' AND '"+endTime+"'"
+	sql := "SELECT time, message FROM client_err "
+	sql += "WHERE time BETWEEN '" + startTime + "' AND '" + endTime + "'"
 
-    log.Infof("sql: %s", sql)
+	log.Infof("sql: %s", sql)
 
-    rows, err := db.Query(sql)
+	rows, err := db.Query(sql)
 	if err != nil {
 		return err
 	}
 	defer rows.Close()
 
-	simpleErrInfo := make([]errSimpleInfo, 0)
-    for rows.Next() {
-        var r errSimpleInfo
-        var sErrTime string
-        if err := rows.Scan(&sErrTime, &r.ErrMessage); err != nil {
-            return err
-        }
-        // 日期按天排序
-        timeFormat := "2006-01-02 15:04:05"
-        tErrtime := common.ParseTimeInLocal(timeFormat, sErrTime)
+	slSimpleErrInfo := make([]errSimpleInfo, 0)
+	for rows.Next() {
+		var r errSimpleInfo
+		var sErrTime string
+		if err := rows.Scan(&sErrTime, &r.ErrMessage); err != nil {
+			return err
+		}
+		// 日期按天排序
+		timeFormat := "2006-01-02 15:04:05"
+		tErrtime := common.ParseTimeInLocal(timeFormat, sErrTime)
 
-        r.ErrTime = tErrtime.Format("2006-01-02")    
-        r.ErrTimes += 1
+		r.ErrTime = tErrtime.Format("2006-01-02")
+		r.ErrTimes += 1
 
-        bFind := false
-        for k,v := range simpleErrInfo {
-            if len(simpleErrInfo) != 0 && v.ErrTime == r.ErrTime && v.ErrMessage == r.ErrMessage {
-                simpleErrInfo[k].ErrTimes += 1
-                bFind = true
-            }
-        }
+		bFind := false
+		for k, v := range slSimpleErrInfo {
+			if len(slSimpleErrInfo) != 0 && v.ErrTime == r.ErrTime && v.ErrMessage == r.ErrMessage {
+				slSimpleErrInfo[k].ErrTimes += 1
+				bFind = true
+			}
+		}
 
-        if !bFind {
-            simpleErrInfo = append(simpleErrInfo, r)
-        }
-    }
+		if !bFind {
+			slSimpleErrInfo = append(slSimpleErrInfo, r)
+		}
+	}
 
-    if err := rows.Err(); err != nil {
-        return err
-    }
+	if err := rows.Err(); err != nil {
+		return err
+	}
 
-    sort.Sort(errSimpleInfoBy(simpleErrInfo))
+	sort.Sort(errSimpleInfoBy(slSimpleErrInfo))
 
-	return ctx.SendArray(simpleErrInfo, len(simpleErrInfo))
+	return ctx.SendArray(slSimpleErrInfo, len(slSimpleErrInfo))
 }
 
 func ErrDetail(c echo.Context) error {
-    ctx := c.(*mid.Context)
-    logs := make([]errSimpleInfo, 0)
+	ctx := c.(*mid.Context)
+	errInfo := ctx.QueryParam("errInfo")
+	errTime := ctx.QueryParam("errTime")
 
-    return ctx.SendArray(logs, len(logs))
+	if errInfo == "" || errTime == "" {
+		return ctx.SendError(-1, "参数非法")
+	}
+
+	timeFormat := "2006-01-02 15:04:05"
+	tErrtime := common.ParseTimeInLocal(timeFormat, errTime)
+	endTime := tErrtime.Date(0, 0, 1)
+
+	sBeginTime := tErrtime.Format(timeFormat)
+	sEndTime := endTime.Format(timeFormat)
+
+	db := cfg.LogDb
+	sql := "SELECT time, message, zoneid, roleid FROM client_err "
+	sql += "WHERE time BETWEEN '" + sBeginTime + "' AND '" + sEndTime + "' AND message = '" + errInfo + "'"
+
+	log.Infof("sql: %s", sql)
+
+	rows, err := db.Query(sql)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	slErrInfo := make([]errInfo, 0)
+
+	for rows.Next() {
+		var r errInfo
+		if err := rows.Scan(&r.ErrTime, &r.ErrInfo, &r.ZoneId, &r.RoleId); err != nil {
+			return err
+		}
+
+		slErrInfo = append(slErrInfo, r)
+	}
+
+	if err := rows.Err(); err != nil {
+		return err
+	}
+
+	sort.Sort(errInfoBy(slErrInfo))
+
+	return ctx.SendArray(slErrInfo, len(slErrInfo))
 }
