ef8fa368008a8d8fbbf763c63fb4f65dc58ac0e1
lzhang <891056049@qq.com>
2021-03-17 18:29:11 +0800

    入库后读取更改

--- backend/api/game/api_client.go
+++ backend/api/game/api_client.go
@@ -3,18 +3,21 @@ package game
 import (
 	"sort"
 
+	"strings"
+
 	"github.com/labstack/echo"
 	"github.com/yellia1989/tex-go/tools/log"
 	"github.com/yellia1989/tex-web/backend/cfg"
 	common "github.com/yellia1989/tex-web/backend/common"
 	mid "github.com/yellia1989/tex-web/backend/middleware"
-    "strings"
 )
 
 type errSimpleInfo struct {
-	ErrTime    string `json:"err_time"`
-	ErrMessage string `json:"err_info"`
-	ErrTimes   uint32 `json:"err_times"`
+	ErrTime       string `json:"err_time"`
+	ErrMessage    string `json:"err_info"`
+	ErrTimes      uint32 `json:"err_times"`
+	ClientVersion string `json:"client_version"`
+	ErrMessageMd5 string `json:"err_info_md5"`
 }
 
 type errSimpleInfoBy []errSimpleInfo
@@ -22,9 +25,13 @@ type errSimpleInfoBy []errSimpleInfo
 func (a errSimpleInfoBy) Len() int      { return len(a) }
 func (a errSimpleInfoBy) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
 func (a errSimpleInfoBy) Less(i, j int) bool {
-    if a[i].ErrTime > a[j].ErrTime {
-        return true
-    }
+	if a[i].ErrTime > a[j].ErrTime {
+		return true
+	}
+
+	if a[i].ClientVersion > a[j].ClientVersion {
+		return true
+	}
 
 	return a[i].ErrTimes > a[j].ErrTimes
 }
@@ -42,12 +49,12 @@ func (a errInfoBy) Len() int      { return len(a) }
 func (a errInfoBy) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
 func (a errInfoBy) Less(i, j int) bool {
 	if a[i].ErrTime > a[j].ErrTime {
-        return true
-    }
+		return true
+	}
 
-    if a[i].ErrMessage > a[j].ErrMessage {
-        return true
-    }
+	if a[i].ErrMessage > a[j].ErrMessage {
+		return true
+	}
 
 	return a[i].ZoneId < a[j].ZoneId
 }
@@ -63,8 +70,12 @@ func ErrInfo(c echo.Context) error {
 
 	db := cfg.LogDb
 
-	sql := "SELECT time, message FROM client_err "
-	sql += "WHERE time BETWEEN '" + startTime + "' AND '" + endTime + "'"
+	timeFormatDay := "2006-01-02"
+	tStartTimeDay := common.ParseTimeInLocal(timeFormatDay, startTime)
+	tEndTime := common.ParseTimeInLocal(timeFormatDay, endTime)
+
+	sql := "SELECT timeymd, client_version, stack, stackmd5 FROM client_err "
+	sql += "WHERE time BETWEEN '" + tStartTime.Format(timeFormatDay) + "' AND '" + tEndTime.Format(timeFormatDay) + "'"
 
 	log.Infof("sql: %s", sql)
 
@@ -77,23 +88,17 @@ func ErrInfo(c echo.Context) error {
 	slSimpleErrInfo := make([]errSimpleInfo, 0)
 	for rows.Next() {
 		var r errSimpleInfo
-		var sErrTime string
-		if err := rows.Scan(&sErrTime, &r.ErrMessage); err != nil {
+		if err := rows.Scan(&r.ErrTime, &r.ClientVersion, &r.ErrMessage, &r.ErrMessageMd5); err != nil {
 			return err
 		}
 
-        r.ErrMessage = strings.Replace(r.ErrMessage, "\n", "<br>", -1)
+		r.ErrMessage = strings.Replace(r.ErrMessage, "\n", "<br>", -1)
 
-		// 日期按天排序
-		timeFormat := "2006-01-02 15:04:05"
-		tErrtime := common.ParseTimeInLocal(timeFormat, sErrTime)
-
-		r.ErrTime = tErrtime.Format("2006-01-02")
 		r.ErrTimes += 1
 
 		bFind := false
 		for k, v := range slSimpleErrInfo {
-			if len(slSimpleErrInfo) != 0 && v.ErrTime == r.ErrTime && v.ErrMessage == r.ErrMessage {
+			if len(slSimpleErrInfo) != 0 && v.ErrTime == r.ErrTime && v.ErrMessageMd5 == r.ErrMessageMd5 {
 				slSimpleErrInfo[k].ErrTimes += 1
 				bFind = true
 			}
@@ -115,28 +120,16 @@ func ErrInfo(c echo.Context) error {
 
 func ErrDetail(c echo.Context) error {
 	ctx := c.(*mid.Context)
-	sErrInfo := ctx.QueryParam("ErrInfo")
+	sErrInfoMd5 := ctx.QueryParam("ErrInfo")
 	sErrTime := ctx.QueryParam("ErrTime")
 
 	if sErrInfo == "" || sErrTime == "" {
 		return ctx.SendError(-1, "参数非法")
 	}
 
-    // 时间转换
-	timeFormat := "2006-01-02 15:04:05"
-    timeFormatDay := "2006-01-02"
-	tErrtime := common.ParseTimeInLocal(timeFormatDay, sErrTime)
-    log.Infof("errTime: %s", tErrtime.Format(timeFormat))
-	endTime := tErrtime.AddDate(0, 0, 1)
-
-	sBeginTime := tErrtime.Format(timeFormat)
-	sEndTime := endTime.Format(timeFormat)
-
-    sErrInfo = strings.Replace(sErrInfo, "<br>", "\n", -1)
-
 	db := cfg.LogDb
-	sql := "SELECT time, message, zoneid, roleid FROM client_err "
-	sql += "WHERE time BETWEEN '" + sBeginTime + "' AND '" + sEndTime + "' AND message = '" + sErrInfo + "'"
+	sql := "SELECT timehms, message, zoneid, roleid FROM client_err "
+	sql += "WHERE str_to_date(timeymd, '%Y-%m-%d') = str_to_date('" + sErrTime + "')  AND stackmd5 = '" + sErrInfoMd5 + "'"
 
 	log.Infof("sql: %s", sql)
 
@@ -153,7 +146,7 @@ func ErrDetail(c echo.Context) error {
 		if err := rows.Scan(&r.ErrTime, &r.ErrMessage, &r.ZoneId, &r.RoleId); err != nil {
 			return err
 		}
-        r.ErrMessage = strings.Replace(r.ErrMessage, "\n", "<br>", -1)
+		r.ErrMessage = strings.Replace(r.ErrMessage, "\n", "<br>", -1)
 
 		slErrInfo = append(slErrInfo, r)
 	}
