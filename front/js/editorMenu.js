b03d6d8bdaca2546cf71fe4e4cd679b0831d097c
zzh34634 <547715735@qq.com>
2021-04-20 21:18:45 +0800

    clean

--- front/js/editorMenu.js
+++ front/js/editorMenu.js
@@ -3,71 +3,6 @@ const E = window.wangEditor
 
 const { $, BtnMenu, DropListMenu, PanelMenu, DropList, Panel, Tooltip } = E
 
-class FonSizeMenu extends PanelMenu{
-
-    constructor(editor) {
-        // data-title属性表示当鼠标悬停在该按钮上时提示该按钮的功能简述
-        const $elem = E.$(
-            `<div class="w-e-menu" data-title="FontSize">
-                <input id="fontSizeInput" style="width: 30px;text-align: center;" type="text" value="22"/>
-            </div>`
-        )
-        super($elem, editor)
-    }
-
-    /**
-     * 菜单点击事件
-     */
-    clickHandler() {
-        const panel = new Panel(this,{width:100,height:100,tabs:[{
-                title:"字体大小",
-                tpl:'<div class="w-e-button-container"><div id="FontConfirm">确定</div></div>',
-                events:[{
-                    selector: '#FontConfirm',
-                    type : 'click',
-                    fn:()=>{
-                        // console.log(editor.selection.getSelectionText());
-                        // console.log(editor.selection.getSelectionContainerElem().elems[0]);
-                        // console.log(editor.selection.getSelectionStartElem().elems[0]);
-                        // console.log(editor.selection.getSelectionEndElem().elems[0]);
-                        // console.log(editor.selection.isSelectionEmpty());
-                        // var rang = editor.selection.getRange();
-                        if(editor.selection.isSelectionEmpty()){
-                            return true;
-                        }
-                        var rang = this.selectRange;
-                        var selectedText = rang.extractContents();
-                        var ele = document.createElement("span");
-                        console.log($("#fontSizeInput").val());
-                        ele.style = "font-size:"+$("#fontSizeInput").val().trim()+"px;";
-                        ele.appendChild(selectedText);
-                        rang.insertNode(ele);
-                        console.log(this.selectRange);
-                        //rang.surroundContents(ele);
-                        return true;
-                    },
-                }],
-            }]});
-        panel.create();
-        this.selectRange = editor.selection.getRange();
-        // $("#FontConfirm").on("click",function(){
-        //   var selectedText = rang.extractContents();
-        //   var ele = document.createElement("span");
-        //   var input = $("#fontSizeInput");
-        //   var size = $("#fontSizeInput").val();
-        //   console.log(size);
-        //   ele.style = "font-size:30px;";
-        //   ele.appendChild(selectedText);
-        //   rang.insertNode(ele);
-        // });
-    }
-
-    tryChangeActive() {
-
-    }
-
-}
-
 class MyColorMenu extends BtnMenu {
     constructor(editor) {
         // data-title属性表示当鼠标悬停在该按钮上时提示该按钮的功能简述
@@ -97,26 +32,6 @@ class MyColorMenu extends BtnMenu {
      * 菜单点击事件
      */
     clickHandler() {
-        console.log("cCLICK");
-        // const panel = new Panel(this,{width:150,height:100,tabs:[{
-        //     title:"颜色",
-        //     tpl:'<div >选择颜色<div id="colorPicker" style="margin-left: 10px"></div></div>',
-        //     events:[{
-        //       selector: '#TestColor',
-        //       type : 'click',
-        //       fn:()=>{
-        //         console.log(editor.selection.getSelectionText());
-        //         console.log(editor.selection.getSelectionContainerElem().elems[0]);
-        //         console.log(editor.selection.getSelectionStartElem().elems[0]);
-        //         console.log(editor.selection.getSelectionEndElem().elems[0]);
-        //         console.log(editor.selection.isSelectionEmpty());
-        //         editor.cmd.do('foreColor', '#FFFFFF');
-        //         editor.cmd.do('cut');
-        //         return true;
-        //       },
-        //     }],
-        //   }]});
-        // panel.create();
 
     }
 
@@ -129,7 +44,6 @@ class MyColorMenu extends BtnMenu {
 
 // 注册菜单
 E.registerMenu('myColorKey', MyColorMenu);
-//E.registerMenu('FonSizeKey',FonSizeMenu);
 
 const fontSizeMap = {'1': '10', '2': '13', '3': '16', '4': '18', '5': '24', '6': '32', '7': '48',};
 
