5cebe864fc78b5e8d9b3bcdc3b82968de45776f6
张艾敬 <57080034+a1364702@users.noreply.github.com>
2021-04-28 19:49:28 +0800

    see #3841 (#51)

--- front/js/activity.config.js
+++ front/js/activity.config.js
@@ -379,7 +379,12 @@
                 recommond: {
                     name: '焦点参数(秒)',
                     type: 'text',
+                },
+                help_id : {
+                    name: '帮助提示',
+                    type: 'text',
                 }
+
             }
         }
     };
@@ -1391,6 +1396,10 @@
                 },
                 recommond: {
                     name: '焦点参数(秒)',
+                    type: 'text',
+                },
+                help_id : {
+                    name: '帮助提示',
                     type: 'text',
                 }
             }
