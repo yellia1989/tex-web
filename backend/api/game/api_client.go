128ca9dd31960f10de281783c769aa658a2c501f
qq891056049 <891056049@qq.com>
2021-05-06 18:24:38 +0800

    * 报警监控增加客户端战斗验证失败页面

--- backend/api/game/api_client.go
+++ backend/api/game/api_client.go
@@ -1,24 +1,25 @@
 package game
 
 import (
-    "sort"
-    "strings"
-    "strconv"
-    "encoding/base64"
-    "github.com/yellia1989/tex-web/backend/common"
-    "github.com/labstack/echo"
-    "github.com/yellia1989/tex-go/tools/log"
-    "github.com/yellia1989/tex-web/backend/cfg"
-    mid "github.com/yellia1989/tex-web/backend/middleware"
+	"encoding/base64"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/labstack/echo"
+	"github.com/yellia1989/tex-go/tools/log"
+	"github.com/yellia1989/tex-web/backend/cfg"
+	"github.com/yellia1989/tex-web/backend/common"
+	mid "github.com/yellia1989/tex-web/backend/middleware"
 )
 
 type errSimpleInfo struct {
-    ErrTime       string `json:"err_time"`
-    ErrMessage    string `json:"err_info"`
-    ErrTimes      uint32 `json:"err_times"`
-    ClientVersion string `json:"client_version"`
-    ErrMessageMd5 string `json:"err_info_md5"`
-    Status        uint32 `json:"status"`
+	ErrTime       string `json:"err_time"`
+	ErrMessage    string `json:"err_info"`
+	ErrTimes      uint32 `json:"err_times"`
+	ClientVersion string `json:"client_version"`
+	ErrMessageMd5 string `json:"err_info_md5"`
+	Status        uint32 `json:"status"`
 }
 
 type errSimpleInfoBy []errSimpleInfo
@@ -26,25 +27,24 @@ type errSimpleInfoBy []errSimpleInfo
 func (a errSimpleInfoBy) Len() int      { return len(a) }
 func (a errSimpleInfoBy) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
 func (a errSimpleInfoBy) Less(i, j int) bool {
-    TmpTimeI := common.ParseTimeInLocal("2006-01-02", a[i].ErrTime)
-    TmpTimeJ := common.ParseTimeInLocal("2006-01-02", a[j].ErrTime)
+	TmpTimeI := common.ParseTimeInLocal("2006-01-02", a[i].ErrTime)
+	TmpTimeJ := common.ParseTimeInLocal("2006-01-02", a[j].ErrTime)
 
+	if a[i].ClientVersion != a[j].ClientVersion {
+		return a[i].ClientVersion > a[j].ClientVersion
+	}
 
-    if a[i].ClientVersion != a[j].ClientVersion {
-        return a[i].ClientVersion > a[j].ClientVersion
-    }
+	if !TmpTimeI.Equal(TmpTimeJ) {
+		return TmpTimeI.After(TmpTimeJ)
+	}
 
-    if !TmpTimeI.Equal(TmpTimeJ) {
-        return TmpTimeI.After(TmpTimeJ)
-    }
-
-    return a[i].ErrTimes > a[j].ErrTimes
+	return a[i].ErrTimes > a[j].ErrTimes
 }
 
 type errInfo struct {
-    ErrTime    string `json:"err_time"`
-    ZoneId     uint32 `json:"zone_id"`
-    RoleId     uint32 `json:"role_id"`
+	ErrTime string `json:"err_time"`
+	ZoneId  uint32 `json:"zone_id"`
+	RoleId  uint32 `json:"role_id"`
 }
 
 type errInfoBy []errInfo
@@ -52,22 +52,22 @@ type errInfoBy []errInfo
 func (a errInfoBy) Len() int      { return len(a) }
 func (a errInfoBy) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
 func (a errInfoBy) Less(i, j int) bool {
-    TmpTimeI := common.ParseTimeInLocal("15:04:05", a[i].ErrTime)
-    TmpTimeJ := common.ParseTimeInLocal("15:04:05", a[j].ErrTime)
+	TmpTimeI := common.ParseTimeInLocal("15:04:05", a[i].ErrTime)
+	TmpTimeJ := common.ParseTimeInLocal("15:04:05", a[j].ErrTime)
 
-    if !TmpTimeI.Equal(TmpTimeJ) {
-        return TmpTimeI.After(TmpTimeJ)
-    }
+	if !TmpTimeI.Equal(TmpTimeJ) {
+		return TmpTimeI.After(TmpTimeJ)
+	}
 
-    return a[i].ZoneId < a[j].ZoneId
+	return a[i].ZoneId < a[j].ZoneId
 }
 
 type disposeInfo struct {
-    ErrMessage     string `json:"err_info"`
-    ErrMessageMd5  string `json:"err_info_md5"`
-    ClientVersion  string `json:"client_version"`
-    Status         uint32 `json:"status"`
-    Note           string `json:"err_note"`
+	ErrMessage    string `json:"err_info"`
+	ErrMessageMd5 string `json:"err_info_md5"`
+	ClientVersion string `json:"client_version"`
+	Status        uint32 `json:"status"`
+	Note          string `json:"err_note"`
 }
 
 type disposeInfoBy []disposeInfo
@@ -75,232 +75,229 @@ type disposeInfoBy []disposeInfo
 func (a disposeInfoBy) Len() int      { return len(a) }
 func (a disposeInfoBy) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
 func (a disposeInfoBy) Less(i, j int) bool {
-    return a[i].ClientVersion > a[j].ClientVersion
+	return a[i].ClientVersion > a[j].ClientVersion
 }
 
-
 func ErrInfo(c echo.Context) error {
-    ctx := c.(*mid.Context)
-    startTime := ctx.QueryParam("startTime")
-    endTime := ctx.QueryParam("endTime")
-    page, _ := strconv.Atoi(ctx.QueryParam("page"))
-    limit, _ := strconv.Atoi(ctx.QueryParam("limit"))
+	ctx := c.(*mid.Context)
+	startTime := ctx.QueryParam("startTime")
+	endTime := ctx.QueryParam("endTime")
+	page, _ := strconv.Atoi(ctx.QueryParam("page"))
+	limit, _ := strconv.Atoi(ctx.QueryParam("limit"))
 
-    if startTime == "" || endTime == "" {
-        return ctx.SendError(-1, "参数非法")
-    }
+	if startTime == "" || endTime == "" {
+		return ctx.SendError(-1, "参数非法")
+	}
 
-    db := cfg.LogDb
+	db := cfg.LogDb
 
-    sql := "SELECT timeymd, client_version, stack, stackmd5, count(*) as count  FROM client_error "
-    sql += "WHERE time BETWEEN '" + startTime + "' AND '" + endTime + "'"
-    sql += "GROUP BY client_version, stackmd5"
+	sql := "SELECT timeymd, client_version, stack, stackmd5, count(*) as count  FROM client_error "
+	sql += "WHERE time BETWEEN '" + startTime + "' AND '" + endTime + "'"
+	sql += "GROUP BY client_version, stackmd5"
 
-    log.Infof("sql: %s", sql)
+	log.Infof("sql: %s", sql)
 
-    rows, err := db.Query(sql)
-    if err != nil {
-        return err
-    }
-    defer rows.Close()
+	rows, err := db.Query(sql)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
 
-    slSimpleErrInfo := make([]errSimpleInfo, 0)
-    for rows.Next() {
-        var r errSimpleInfo
-        if err := rows.Scan(&r.ErrTime, &r.ClientVersion, &r.ErrMessage, &r.ErrMessageMd5, &r.ErrTimes); err != nil {
-            return err
-        }
+	slSimpleErrInfo := make([]errSimpleInfo, 0)
+	for rows.Next() {
+		var r errSimpleInfo
+		if err := rows.Scan(&r.ErrTime, &r.ClientVersion, &r.ErrMessage, &r.ErrMessageMd5, &r.ErrTimes); err != nil {
+			return err
+		}
 
-        decodeBytes, _ := base64.StdEncoding.DecodeString(r.ErrMessage)
-        r.ErrMessage = string(decodeBytes)
-        r.ErrMessage = strings.Replace(r.ErrMessage, "\n", "<br>", -1)
+		decodeBytes, _ := base64.StdEncoding.DecodeString(r.ErrMessage)
+		r.ErrMessage = string(decodeBytes)
+		r.ErrMessage = strings.Replace(r.ErrMessage, "\n", "<br>", -1)
 
-        slSimpleErrInfo = append(slSimpleErrInfo, r)
-    }
+		slSimpleErrInfo = append(slSimpleErrInfo, r)
+	}
 
-    if err := rows.Err(); err != nil {
-        return err
-    }
+	if err := rows.Err(); err != nil {
+		return err
+	}
 
-    db = cfg.StatDb
-    sql1 := "SELECT status, client_version, stackmd5 FROM client_dispose "
-    rows1, err1 := db.Query(sql1)
-    if err1 != nil {
-        return err1
-    }
-    defer rows1.Close()
+	db = cfg.StatDb
+	sql1 := "SELECT status, client_version, stackmd5 FROM client_dispose "
+	rows1, err1 := db.Query(sql1)
+	if err1 != nil {
+		return err1
+	}
+	defer rows1.Close()
 
-    slDisposeInfo := make([]disposeInfo, 0)
-    for rows1.Next() {
-        var r disposeInfo
-        if err2 := rows1.Scan(&r.Status, &r.ClientVersion, &r.ErrMessageMd5); err != nil {
-            return err2
-        }
+	slDisposeInfo := make([]disposeInfo, 0)
+	for rows1.Next() {
+		var r disposeInfo
+		if err2 := rows1.Scan(&r.Status, &r.ClientVersion, &r.ErrMessageMd5); err != nil {
+			return err2
+		}
 
-        slDisposeInfo = append(slDisposeInfo, r)
-    }
+		slDisposeInfo = append(slDisposeInfo, r)
+	}
 
-    for k, errInfo := range slSimpleErrInfo {
-        bFind := false
-        for _, disInfo := range slDisposeInfo {
-            if errInfo.ErrMessageMd5 == disInfo.ErrMessageMd5 && errInfo.ClientVersion == disInfo.ClientVersion {
-               slSimpleErrInfo[k].Status = disInfo.Status
-               bFind = true;
-            }
-        }
-        if (!bFind) {
-            slSimpleErrInfo[k].Status = 1;
-        }
-    }
+	for k, errInfo := range slSimpleErrInfo {
+		bFind := false
+		for _, disInfo := range slDisposeInfo {
+			if errInfo.ErrMessageMd5 == disInfo.ErrMessageMd5 && errInfo.ClientVersion == disInfo.ClientVersion {
+				slSimpleErrInfo[k].Status = disInfo.Status
+				bFind = true
+			}
+		}
+		if !bFind {
+			slSimpleErrInfo[k].Status = 1
+		}
+	}
 
+	sort.Sort(errSimpleInfoBy(slSimpleErrInfo))
 
-    sort.Sort(errSimpleInfoBy(slSimpleErrInfo))
+	vSimpleErrInfo := common.GetPage(slSimpleErrInfo, page, limit)
 
-    vSimpleErrInfo := common.GetPage(slSimpleErrInfo, page, limit)
-
-    return ctx.SendArray(vSimpleErrInfo, len(slSimpleErrInfo))
+	return ctx.SendArray(vSimpleErrInfo, len(slSimpleErrInfo))
 }
 
 func ErrDetail(c echo.Context) error {
-    ctx := c.(*mid.Context)
-    sErrInfoMd5 := ctx.QueryParam("ErrInfo")
-    sClientVersion := ctx.QueryParam("ClientVersion")
-    page, _ := strconv.Atoi(ctx.QueryParam("page"))
-    limit, _ := strconv.Atoi(ctx.QueryParam("limit"))
+	ctx := c.(*mid.Context)
+	sErrInfoMd5 := ctx.QueryParam("ErrInfo")
+	sClientVersion := ctx.QueryParam("ClientVersion")
+	page, _ := strconv.Atoi(ctx.QueryParam("page"))
+	limit, _ := strconv.Atoi(ctx.QueryParam("limit"))
 
-    if sErrInfoMd5 == "" || sClientVersion == "" {
-        return ctx.SendError(-1, "参数非法")
-    }
+	if sErrInfoMd5 == "" || sClientVersion == "" {
+		return ctx.SendError(-1, "参数非法")
+	}
 
-    db := cfg.LogDb
-    sql := "SELECT time, zoneid, roleid FROM client_error "
-    sql += "WHERE stackmd5 = '" + sErrInfoMd5 + "'"
-    sql += "AND client_version = '"+ sClientVersion + "'"
+	db := cfg.LogDb
+	sql := "SELECT time, zoneid, roleid FROM client_error "
+	sql += "WHERE stackmd5 = '" + sErrInfoMd5 + "'"
+	sql += "AND client_version = '" + sClientVersion + "'"
 
-    log.Infof("sql: %s", sql)
+	log.Infof("sql: %s", sql)
 
-    rows, err := db.Query(sql)
-    if err != nil {
-        return err
-    }
-    defer rows.Close()
+	rows, err := db.Query(sql)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
 
-    slErrInfo := make([]errInfo, 0)
+	slErrInfo := make([]errInfo, 0)
 
-    for rows.Next() {
-        var r errInfo
-        if err := rows.Scan(&r.ErrTime, &r.ZoneId, &r.RoleId); err != nil {
-            return err
-        }
+	for rows.Next() {
+		var r errInfo
+		if err := rows.Scan(&r.ErrTime, &r.ZoneId, &r.RoleId); err != nil {
+			return err
+		}
 
-        slErrInfo = append(slErrInfo, r)
-    }
+		slErrInfo = append(slErrInfo, r)
+	}
 
-    if err := rows.Err(); err != nil {
-        return err
-    }
+	if err := rows.Err(); err != nil {
+		return err
+	}
 
-    sort.Sort(errInfoBy(slErrInfo))
-    vErrInfo := common.GetPage(slErrInfo, page, limit)
+	sort.Sort(errInfoBy(slErrInfo))
+	vErrInfo := common.GetPage(slErrInfo, page, limit)
 
-
-    return ctx.SendArray(vErrInfo, len(slErrInfo))
+	return ctx.SendArray(vErrInfo, len(slErrInfo))
 }
 
 func ErrDispose(c echo.Context) error {
-    ctx := c.(*mid.Context)
-    sErrInfo := ctx.FormValue("sErrInfo")
-    sErrInfoMd5 := ctx.FormValue("sErrInfoMd5")
-    sClientVersion := ctx.FormValue("sClientVersion")
-    if sErrInfoMd5 == "" || sClientVersion == "" || sErrInfo == "" {
-        return ctx.SendError(-1, "参数非法")
-    }
-    db := cfg.StatDb
+	ctx := c.(*mid.Context)
+	sErrInfo := ctx.FormValue("sErrInfo")
+	sErrInfoMd5 := ctx.FormValue("sErrInfoMd5")
+	sClientVersion := ctx.FormValue("sClientVersion")
+	if sErrInfoMd5 == "" || sClientVersion == "" || sErrInfo == "" {
+		return ctx.SendError(-1, "参数非法")
+	}
+	db := cfg.StatDb
 
-    sql := "INSERT INTO client_dispose (stack, stackmd5, client_version, status, note) VALUES (?,?,?,?,?)"
-    rows, err := db.Query(sql, sErrInfo, sErrInfoMd5, sClientVersion, 2, "")
-    if err != nil {
-        return err
-    }
-    defer rows.Close()
+	sql := "INSERT INTO client_dispose (stack, stackmd5, client_version, status, note) VALUES (?,?,?,?,?)"
+	rows, err := db.Query(sql, sErrInfo, sErrInfoMd5, sClientVersion, 2, "")
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
 
-    return ctx.SendResponse("添加处理成功")
+	return ctx.SendResponse("添加处理成功")
 }
 
 func FinishDispose(c echo.Context) error {
-    ctx := c.(*mid.Context)
-    sErrInfoMd5 := ctx.FormValue("sErrInfoMd5")
-    sClientVersion := ctx.FormValue("sClientVersion")
-    if sErrInfoMd5 == "" || sClientVersion == "" {
-        return ctx.SendError(-1, "参数非法")
-    }
+	ctx := c.(*mid.Context)
+	sErrInfoMd5 := ctx.FormValue("sErrInfoMd5")
+	sClientVersion := ctx.FormValue("sClientVersion")
+	if sErrInfoMd5 == "" || sClientVersion == "" {
+		return ctx.SendError(-1, "参数非法")
+	}
 
-    db := cfg.StatDb
+	db := cfg.StatDb
 
-    sql := "UPDATE client_dispose SET status=3 WHERE client_version=? AND stackmd5=? "
-    rows, err := db.Query(sql, sClientVersion, sErrInfoMd5)
-    if err != nil {
-        return err
-    }
-    defer rows.Close()
+	sql := "UPDATE client_dispose SET status=3 WHERE client_version=? AND stackmd5=? "
+	rows, err := db.Query(sql, sClientVersion, sErrInfoMd5)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
 
-    return ctx.SendResponse("更新处理状态成功")
+	return ctx.SendResponse("更新处理状态成功")
 }
 
 func DisposeList(c echo.Context) error {
-    ctx := c.(*mid.Context)
-    page, _ := strconv.Atoi(ctx.QueryParam("page"))
-    limit, _ := strconv.Atoi(ctx.QueryParam("limit"))
+	ctx := c.(*mid.Context)
+	page, _ := strconv.Atoi(ctx.QueryParam("page"))
+	limit, _ := strconv.Atoi(ctx.QueryParam("limit"))
 
-    db := cfg.StatDb
-    sql := "SELECT client_version, stack, stackmd5, status, note FROM client_dispose "
+	db := cfg.StatDb
+	sql := "SELECT client_version, stack, stackmd5, status, note FROM client_dispose "
 
-    log.Infof("sql: %s", sql)
+	log.Infof("sql: %s", sql)
 
-    rows, err := db.Query(sql)
-    if err != nil {
-        return err
-    }
-    defer rows.Close()
+	rows, err := db.Query(sql)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
 
-    slDisposeInfo := make([]disposeInfo, 0)
+	slDisposeInfo := make([]disposeInfo, 0)
 
-    for rows.Next() {
-        var r disposeInfo
-        if err := rows.Scan(&r.ClientVersion, &r.ErrMessage, &r.ErrMessageMd5, &r.Status, &r.Note); err != nil {
-            return err
-        }
+	for rows.Next() {
+		var r disposeInfo
+		if err := rows.Scan(&r.ClientVersion, &r.ErrMessage, &r.ErrMessageMd5, &r.Status, &r.Note); err != nil {
+			return err
+		}
 
-        slDisposeInfo = append(slDisposeInfo, r)
-    }
+		slDisposeInfo = append(slDisposeInfo, r)
+	}
 
-    if err := rows.Err(); err != nil {
-        return err
-    }
+	if err := rows.Err(); err != nil {
+		return err
+	}
 
-    sort.Sort(disposeInfoBy(slDisposeInfo))
-    vDisposeInfo:= common.GetPage(slDisposeInfo, page, limit)
+	sort.Sort(disposeInfoBy(slDisposeInfo))
+	vDisposeInfo := common.GetPage(slDisposeInfo, page, limit)
 
-    return ctx.SendArray(vDisposeInfo, len(slDisposeInfo))
+	return ctx.SendArray(vDisposeInfo, len(slDisposeInfo))
 }
 
 func AddDisposeNote(c echo.Context) error {
-    ctx := c.(*mid.Context)
-    sErrInfoMd5 := ctx.FormValue("sErrInfoMd5")
-    sClientVersion := ctx.FormValue("sClientVersion")
-    sNote := ctx.FormValue("sNote")
-    if sErrInfoMd5 == "" || sClientVersion == "" || sNote == ""{
-        return ctx.SendError(-1, "参数非法")
-    }
+	ctx := c.(*mid.Context)
+	sErrInfoMd5 := ctx.FormValue("sErrInfoMd5")
+	sClientVersion := ctx.FormValue("sClientVersion")
+	sNote := ctx.FormValue("sNote")
+	if sErrInfoMd5 == "" || sClientVersion == "" || sNote == "" {
+		return ctx.SendError(-1, "参数非法")
+	}
 
-    db := cfg.StatDb
+	db := cfg.StatDb
 
-    sql := "UPDATE  client_dispose SET note=? WHERE client_version=? AND stackmd5=? "
-    rows, err := db.Query(sql, sNote, sClientVersion, sErrInfoMd5)
-    if err != nil {
-        return err
-    }
-    defer rows.Close()
+	sql := "UPDATE  client_dispose SET note=? WHERE client_version=? AND stackmd5=? "
+	rows, err := db.Query(sql, sNote, sClientVersion, sErrInfoMd5)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
 
-    return ctx.SendResponse("添加备注成功")
+	return ctx.SendResponse("添加备注成功")
 }
---
+++ backend/api/game/api_fight.go
@@ -0,0 +1,90 @@
+package game
+
+import (
+	"encoding/base64"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/labstack/echo"
+	"github.com/yellia1989/tex-go/tools/log"
+	"github.com/yellia1989/tex-web/backend/cfg"
+	"github.com/yellia1989/tex-web/backend/common"
+	mid "github.com/yellia1989/tex-web/backend/middleware"
+)
+
+type fightVerifyErrInfo struct {
+	ErrTime         string `json:"err_time"`
+	ClientVerify    string `json:"client_verify"`
+	ServerVerify    string `json:"server_verify"`
+	ClientVerifyMd5 string `json:"client_verify_md5"`
+	ServerVerifyMd5 string `json:"server_verify_md5"`
+}
+
+type fightVerifyErrInfoBy []fightVerifyErrInfo
+
+func (a fightVerifyErrInfoBy) Len() int      { return len(a) }
+func (a fightVerifyErrInfoBy) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a fightVerifyErrInfoBy) Less(i, j int) bool {
+	TmpTimeI := common.ParseTimeInLocal("2006-01-02", a[i].ErrTime)
+	TmpTimeJ := common.ParseTimeInLocal("2006-01-02", a[j].ErrTime)
+
+	if !TmpTimeI.Equal(TmpTimeJ) {
+		return TmpTimeI.After(TmpTimeJ)
+	}
+
+	return a[i].ErrTime > a[j].ErrTime
+}
+
+func ErrInfo(c echo.Context) error {
+	ctx := c.(*mid.Context)
+	startTime := ctx.QueryParam("startTime")
+	endTime := ctx.QueryParam("endTime")
+	page, _ := strconv.Atoi(ctx.QueryParam("page"))
+	limit, _ := strconv.Atoi(ctx.QueryParam("limit"))
+
+	if startTime == "" || endTime == "" {
+		return ctx.SendError(-1, "参数非法")
+	}
+
+	db := cfg.LogDb
+
+	sql := "SELECT timeymd, client_verify, server_verify, client_verify_md5, server_verify_md5 FROM fight_verify "
+	sql += "WHERE time BETWEEN '" + startTime + "' AND '" + endTime + "'"
+
+	log.Infof("sql: %s", sql)
+
+	rows, err := db.Query(sql)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	slSimpleErrInfo := make([]fightVerifyErrInfo, 0)
+	for rows.Next() {
+		var r fightVerifyErrInfo
+		if err := rows.Scan(&r.ErrTime, &r.ClientVerify, &r.ServerVerify, &r.ClientVerifyMd5, &r.ServerVerifyMd5); err != nil {
+			return err
+		}
+
+		decodeBytes, _ := base64.StdEncoding.DecodeString(r.ClientVerify)
+		r.ClientVerify = string(decodeBytes)
+		r.ClientVerify = strings.Replace(r.ClientVerify, "\n", "<br>", -1)
+
+		decodeBytes1, _ := base64.StdEncoding.DecodeString(r.ServerVerify)
+		r.ServerVerify = string(decodeBytes1)
+		r.ServerVerify = strings.Replace(r.ServerVerify, "\n", "<br>", -1)
+
+		slSimpleErrInfo = append(slSimpleErrInfo, r)
+	}
+
+	if err := rows.Err(); err != nil {
+		return err
+	}
+
+	sort.Sort(fightVerifyErrInfoBy(slSimpleErrInfo))
+
+	vSimpleErrInfo := common.GetPage(slSimpleErrInfo, page, limit)
+
+	return ctx.SendArray(vSimpleErrInfo, len(slSimpleErrInfo))
+}
