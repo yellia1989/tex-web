3f05f55363374bbda9783f869d5cc242d80f1500
LazyFishDream <1719472077@qq.com>
2021-09-27 14:59:47 +0800

    2

--- backend/api/game/zone.go
+++ backend/api/game/zone.go
@@ -1,12 +1,13 @@
 package game
 
 import (
+    "fmt"
     "database/sql"
     "github.com/yellia1989/tex-web/backend/cfg"
 )
 
 func zoneLogDb(zoneid string) (*sql.DB,error) {
-    /*db := cfg.StatDb
+    db := cfg.StatDb
 
     var dbhost sql.NullString
     if err := db.QueryRow("SELECT logdbhost FROM zone WHERE zoneid = " + zoneid).Scan(&dbhost); err != nil {
@@ -14,6 +15,4 @@ func zoneLogDb(zoneid string) (*sql.DB,error) {
     }
 
     return sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:3306)/log_zone_%s", cfg.LogDbUser, cfg.LogDbPwd, dbhost.String, zoneid))
-    */
-    return cfg.LogDb,nil
 }
