429f604dd457f9051239ba76991296ac8dd1c12a
lzhang <891056049@qq.com>
2021-10-11 09:52:25 +0800

    1

--- front/js/activity.config.js
+++ front/js/activity.config.js
@@ -869,7 +869,7 @@
                         '6': '技能升级',
                         '7': '英雄升级',
                         '8': '购买符文',
-                        '9': '英雄历练升级',
+                        '9': '英雄历练升级或更新怪校验',
                         '10': '主线任务进度变化',
                         '11': '商店购买',
                         '12': '蓝钻商城购买',
@@ -1021,6 +1021,24 @@
                 },
                 exp_less_herolevelcost: {
                     name: '当前药水量小于上次英雄升级所消耗的药水量',
+                    type: 'select',
+                    options: options.optYesNo,
+                    parser: parseInt
+                },
+                gem_less_refreshcardcost: {
+                    name: '当前拥有源质琉璃小于上次历练升级或更换怪所消耗的量',
+                    type: 'select',
+                    options: options.optYesNo,
+                    parser: parseInt
+                },
+                rune_less_buyrunecost: {
+                    name: '当前拥有符文晶块小于上次购买符文所消耗的量',
+                    type: 'select',
+                    options: options.optYesNo,
+                    parser: parseInt
+                },
+                skillponit_less_talentlevelupcost: {
+                    name: '当前拥有提纯试剂小于上次天赋升级所消耗的量',
                     type: 'select',
                     options: options.optYesNo,
                     parser: parseInt
