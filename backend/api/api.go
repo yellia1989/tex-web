7ae2e0612cb25f58e18974581213a0f3f3983faa
leizhao <33220203+LazyFishDream@users.noreply.github.com>
2021-11-10 10:47:09 +0800

    模板管理 (#98)
    * test
    
    * 2
    
    * 2
    
    * 2
    
    * 搜索服务
    
    * 获取详情
    
    * 2
    
    * 2
    
    * 2
    
    * 2
    
    * 任务详情
    
    * 批量处理
    
    * 恢复
    
    * 模板管理
    
    * 2
    
    * 父模板选择 增加模板
    
    * 2
    
    * fmt
    
    Co-authored-by: leizhao <zhao.lei@vaneson.com>

--- backend/api/api.go
+++ backend/api/api.go
@@ -187,4 +187,10 @@ func RegisterHandler(group *echo.Group) {
     group.GET("/server/list", server.ServerList) // 获取服务器列表
     group.POST("/server/operator", server.ServerOperator) // 操作服务器
     group.GET("/server/getTask", server.GetTask) // 获取执行信息
+
+    group.GET("/server/template_list", server.TemplateList) // 获取模板列表
+    group.GET("/server/template_detail", server.TemplateDatail) // 获取模板详情
+    group.POST("/server/template_update", server.TemplateUpdate) // 编辑模板
+    group.POST("/server/template_del", server.TemplateDel) // 删除模板
+    group.POST("/server/template_add", server.TemplateAdd) // 增加模板
 }
