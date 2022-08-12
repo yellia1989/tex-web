464159a57edb9a1f590510af8f1d2b65c5e1b0b3
leizhao <zhao.lei@vaneson.com>
2022-08-12 18:15:40 +0800

    2

--- backend/api/api.go
+++ backend/api/api.go
@@ -175,6 +175,9 @@ func RegisterHandler(group *echo.Group) {
     group.POST("/gm/res/add_res_action", gm.ResAppendAction)      // 增加获取途径项
     group.GET("/gm/res_num_err/err_info", gm.ResNumErrInfo)       // 资源数量获取异常异常信息
     group.GET("/gm/res_num_err/err_detail", gm.ResNumErrDetail)
+    group.GET("/gm/global_config/list", gm.GlobalConfigList)
+    group.GET("/gm/global_config/add", gm.GlobalConfigAdd)
+    group.GET("/gm/global_config/del", gm.GlobalConfigDel)
 
     group.GET("/game/fight-verify/err_info", game.FightErrInfo) // 战斗验证失败日志列表
     group.POST("/game/fight-verify/export-report", game.FightExportReport) // 导出战斗日志
---
+++ backend/api/gm/api_global_config.go
@@ -0,0 +1,148 @@
+package gm
+
+import (
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+	"github.com/yellia1989/tex-web/backend/cfg"
+	mid "github.com/yellia1989/tex-web/backend/middleware"
+)
+
+type globalConfig struct {
+	SKey   string `json:"sKey"`
+	SValue string `json:"sValue"`
+}
+
+func GlobalConfigList(c echo.Context) error {
+	ctx := c.(*mid.Context)
+	page, _ := strconv.Atoi(ctx.QueryParam("page"))
+	limit, _ := strconv.Atoi(ctx.QueryParam("limit"))
+
+	db := cfg.GameGlobalDb
+	if db == nil {
+		return ctx.SendError(-1, "连接数据库失败")
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	_, err = tx.Exec("USE " + cfg.GameDbPrefix + "db_zone_global")
+	if err != nil {
+		return err
+	}
+
+	sql := "SELECT skey,value FROM t_config"
+	var total int
+	err = tx.QueryRow("SELECT count(*) as total FROM (" + sql + ") a").Scan(&total)
+	if err != nil {
+		return err
+	}
+
+	limitstart := strconv.Itoa((page - 1) * limit)
+	limitrow := strconv.Itoa(limit)
+	sql += " LIMIT " + limitstart + "," + limitrow
+
+	c.Logger().Debug(sql)
+
+	rows, err := tx.Query(sql)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	vRet := make([]globalConfig, 0)
+	for rows.Next() {
+		var r globalConfig
+		if err := rows.Scan(&r.SKey, &r.SValue); err != nil {
+			return err
+		}
+		vRet = append(vRet, r)
+	}
+
+	if err := rows.Err(); err != nil {
+		return err
+	}
+
+	if err := tx.Commit(); err != nil {
+		return err
+	}
+
+	return ctx.SendArray(vRet, total)
+}
+
+func GlobalConfigAdd(c echo.Context) error {
+	ctx := c.(*mid.Context)
+	stConfig := globalConfig{}
+	stConfig.SKey = ctx.FormValue("sKey")
+	stConfig.SValue = ctx.FormValue("sValue")
+
+	if stConfig.SKey == "" || stConfig.SValue == "" {
+		return ctx.SendError(-1, "参数非法")
+	}
+
+	db := cfg.GameGlobalDb
+	if db == nil {
+		return ctx.SendError(-1, "连接数据库失败")
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	_, err = tx.Exec("USE " + cfg.GameDbPrefix + "db_zone_global")
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec("INSERT INTO t_config(skey,value) VALUES(?,?)", stConfig.SKey, stConfig.SValue)
+	if err != nil {
+		return err
+	}
+
+	if err := tx.Commit(); err != nil {
+		return err
+	}
+
+	return ctx.SendResponse("添加配置成功")
+}
+
+func GlobalConfigDel(c echo.Context) error {
+	ctx := c.(*mid.Context)
+
+	keys := ctx.FormValue("keys")
+	if keys == "" {
+		return ctx.SendError(-1, "连接数据库失败")
+	}
+
+	db := cfg.GameGlobalDb
+	if db == nil {
+		return ctx.SendError(-1, "连接数据库失败")
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	_, err = tx.Exec("USE " + cfg.GameDbPrefix + "db_zone_global")
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec("DELETE FROM t_config WHERE skey IN (?)", keys)
+	if err != nil {
+		return err
+	}
+
+	if err := tx.Commit(); err != nil {
+		return err
+	}
+
+	return ctx.SendResponse("删除配置成功")
+}
