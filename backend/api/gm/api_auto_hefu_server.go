5d3d7d30569eeb270654abf25e00c3064ff6c5ac
yellia <865756739@qq.com>
2023-02-09 03:41:04 +0000

    1

--- backend/api/gm/api_auto_hefu_server.go
+++ backend/api/gm/api_auto_hefu_server.go
@@ -17,6 +17,7 @@ type AutoHefuServer struct {
     SSrcZoneIds string `json:"sSrcZoneIds"`
     IMergeToZoneId int `json:"iMergeToZoneId"`
     SHefuTime string `json:"sHefuTime"`
+    SPrepareTime string `json:"sPrepareTime"`
     SCreateTime string `json:"sCreateTime"`
     SStartTime string `json:"sStartTime"`
     SEndTime string `json:"sEndTime"`
@@ -41,7 +42,7 @@ func AutoHefuServerList(c echo.Context) error {
 		return ctx.SendError(-1, "连接数据库失败")
 	}
 
-	sql := "SELECT id,status,src_zoneids,to_zoneid,create_time,hefu_time,start_time,end_time,result FROM t_hefu"
+	sql := "SELECT id,status,src_zoneids,to_zoneid,create_time,prepare_time,hefu_time,start_time,end_time,result FROM t_hefu"
     sql += " ORDER BY id desc"
     var total int
     err := db.QueryRow("SELECT count(*) as total FROM ("+sql+") a").Scan(&total)
@@ -68,14 +69,16 @@ func AutoHefuServerList(c echo.Context) error {
         var sResult sq.NullString
         var sCreateTime sq.NullString
         var sHefuTime sq.NullString
+        var sPrepareTime sq.NullString
         var sStartTime sq.NullString
         var sEndTime sq.NullString
-		if err := rows.Scan(&r.IId, &r.IStatus, &sSrcZoneIds, &r.IMergeToZoneId, &sCreateTime, &sHefuTime, &sStartTime, &sEndTime, &sResult); err != nil {
+		if err := rows.Scan(&r.IId, &r.IStatus, &sSrcZoneIds, &r.IMergeToZoneId, &sCreateTime, &sPrepareTime, &sHefuTime, &sStartTime, &sEndTime, &sResult); err != nil {
 			return err
 		}
         r.SSrcZoneIds = sSrcZoneIds.String
         r.SResult = sResult.String
         r.SCreateTime = sCreateTime.String
+        r.SPrepareTime = sPrepareTime.String
         r.SHefuTime = sHefuTime.String
         r.SStartTime = sStartTime.String
         r.SEndTime = sEndTime.String
@@ -94,8 +97,9 @@ func AutoHefuServerAdd(c echo.Context) error {
     sSrcZoneIds := ctx.FormValue("sSrcZoneIds")
     sMergeToZoneId := ctx.FormValue("iToZoneId")
     sHefuTime := ctx.FormValue("sHefuTime")
+    sPrepareTime := ctx.FormValue("sPrepareTime")
 
-    if sSrcZoneIds == "" || sMergeToZoneId == "" ||  sHefuTime == "" {
+    if sSrcZoneIds == "" || sMergeToZoneId == "" ||  sHefuTime == "" || sPrepareTime == "" {
         return ctx.SendError(-1, "参数非法")
     }
 
@@ -104,9 +108,15 @@ func AutoHefuServerAdd(c echo.Context) error {
 		return ctx.SendError(-1, "连接数据库失败")
 	}
 
+    iPrepareTime := parseTime(sPrepareTime)
+    iHefuTime := parseTime(sHefuTime)
+    if iHefuTime < iPrepareTime {
+        return ctx.SendError(-1, "合服时间不能小于展示时间")
+    }
+
     iMergeToZoneId := common.Atoi(sMergeToZoneId)
     now := time.Now().Unix()
-    _, err := db.Exec("INSERT INTO t_hefu(src_zoneids, to_zoneid, hefu_time, create_time) VALUES(?,?,?,?)", sSrcZoneIds, iMergeToZoneId, formatTime(parseTime(sHefuTime)), formatTime(now))
+    _, err := db.Exec("INSERT INTO t_hefu(src_zoneids, to_zoneid, prepare_time, hefu_time, create_time) VALUES(?,?,?,?,?)", sSrcZoneIds, iMergeToZoneId, formatTime(iPrepareTime), formatTime(iHefuTime), formatTime(now))
 	if err != nil {
 		return err
 	}
@@ -146,6 +156,7 @@ func AutoHefuServerUpdate(c echo.Context) error {
     sSrcZoneIds := ctx.FormValue("sSrcZoneIds")
     sMergeToZoneId := ctx.FormValue("iToZoneId")
     sHefuTime := ctx.FormValue("sHefuTime")
+    sPrepareTime := ctx.FormValue("sPrepareTime")
 
     if sSrcZoneIds == "" || sMergeToZoneId == "" || sHefuTime == "" {
         return ctx.SendError(-1, "参数非法")
@@ -157,7 +168,7 @@ func AutoHefuServerUpdate(c echo.Context) error {
 	}
 
     // 正在进行中或已经合服结束的不能修改
-	sql := "SELECT id FROM t_hefu WHERE status = 0 and id = " + id
+	sql := "SELECT id FROM t_hefu WHERE (status = 0 or status = 3) and id = " + id
     var total int
     err := db.QueryRow("SELECT count(*) as total FROM ("+sql+") a").Scan(&total)
     if err != nil {
@@ -167,8 +178,14 @@ func AutoHefuServerUpdate(c echo.Context) error {
         return ctx.SendError(-1, "任务已结束或正在进行中")
     }
 
+    iPrepareTime := parseTime(sPrepareTime)
+    iHefuTime := parseTime(sHefuTime)
+    if iHefuTime < iPrepareTime {
+        return ctx.SendError(-1, "合服时间不能小于展示时间")
+    }
+
     iMergeToZoneId := common.Atoi(sMergeToZoneId)
-    _, err = db.Exec("UPDATE t_hefu SET src_zoneids=?,to_zoneid=?,hefu_time=? WHERE id=?", sSrcZoneIds, iMergeToZoneId, formatTime(parseTime(sHefuTime)), id)
+    _, err = db.Exec("UPDATE t_hefu SET src_zoneids=?,to_zoneid=?,prepare_time=?,hefu_time=? WHERE id=?", sSrcZoneIds, iMergeToZoneId, formatTime(iPrepareTime), formatTime(iHefuTime), id)
 	if err != nil {
 		return err
 	}
